5f2ac70969d0eaf08f004758b558c79f1dda5872
AQUAPURE-FRANCE <52166529+AQUAPURE-FRANCE@users.noreply.github.com>
2020-11-05 09:51:17 +0100

    Update SymfonyModuleManagerTrait.php

--- modules/symfonymoduletemplate/src/Manager/SymfonyModuleManagerTrait.php
+++ modules/symfonymoduletemplate/src/Manager/SymfonyModuleManagerTrait.php
@@ -1,1 +1,18 @@
+<?php
 
+namespace SymfonyModule\Manager;
+
+trait SymfonyModuleManagerTrait
+{
+    /**
+     * @param string $serviceId
+     * @return false|object|null
+     */
+    public function service(string $serviceId)
+    {
+        global $kernel;
+        $kernel = new \AppKernel("prod", false);
+        $kernel->boot();
+        return $kernel->getContainer()->get($serviceId);
+    }
+}
